abf6ca5ac20277ab9e26b082772faae5c985c42a
Nikolay Plyusnin <nikolay.plyusnin@jetbrains.com>
2020-12-09 16:25:47 +0300

    added auto responding

--- js/lab7.js
+++ js/lab7.js
@@ -19,8 +19,36 @@ function add_mes(evt) {
     mes_window.appendChild(elem);
     input_box.value = "";
     mes_window.scrollBy(0, 1000);
+
+    let responseText = create_response();
+    elem = document.createElement('div');
+    elem.innerHTML =
+        '<div class="delete"><img src="img/delete-message.png" alt=""></div>' +
+        '<p></p>';
+
+    elem.firstChild.firstChild.addEventListener('click', () => delete_message(elem));
+    elem.lastChild.textContent = responseText;
+    elem.classList.add('left-mes');
+    mes_window.appendChild(elem);
+    input_box.value = "";
+    mes_window.scrollBy(0, 1000);
 }
 
+function create_response() {
+    return get_subj() + get_adj() + get_complement();
+}
+
+function get_subj(){
+    return Math.random() > 0.5 ? "I'm a " : "You are a ";
+}
+
+function get_adj(){
+    return Math.random() > 0.5 ? "good " : "bad ";
+}
+
+function get_complement(){
+    return Math.random() > 0.5 ? "human." : "machine.";
+}
 
 function delete_message(elem) {
     elem.remove();
